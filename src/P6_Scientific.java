6a85c62c4b6278bb35365d4bad59310b24f78631
jhitpas1 <jhitpas1@live.maryville.edu>
2017-09-27 16:11:49 -0500

    I figured out how to complete it.

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -7,16 +7,15 @@
 public class P6_Scientific {
 
 	public static void main(String[] args) {
-		// REMINDER: Any print statements should only be in the main method
 	
-			double base = 6.5;
-			int exp= 3;
+			double base = 7.2;
+			int exp= 4;
 			double scientificoutput = computeScientificValue (base, exp);
 			System.out.println(scientificoutput);
 		}
 
 		public static double computeScientificValue(double x, double y) {
-		      z = x * Math.pow(10,y);
+			double z = x * Math.pow(10,y);
 		  
 		      return z;
 	}
