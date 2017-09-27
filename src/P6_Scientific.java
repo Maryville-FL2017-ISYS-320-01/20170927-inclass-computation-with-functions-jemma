708cbba3eff66f2789a6c8c37d79ed955e187ad4
ecklenke <eklenke2@live.maryville.edu>
2017-09-27 16:09:33 -0500

    Worked on 6

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -8,7 +8,17 @@ public class P6_Scientific {
 
 	public static void main(String[] args) {
 		// REMINDER: Any print statements should only be in the main method
+	
+			double base = 6.5;
+			int exp= 3;
+			double scientificoutput = computeScientificValue (base, exp);
+			System.out.println(scientificoutput);
+		}
 
+		public static double computeScientificValue(double x, double y) {
+		      z = x * Math.pow(10,y);
+		  
+		      return z;
 	}
-
 }
+
